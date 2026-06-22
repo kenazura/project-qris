fff2500fde0006862f4b996839a1ae7b815c7448
kenazura <mydchanel1@gmail.com>
2026-06-22 12:09:14 +0700

    Fix structure for Vercel PHP runtime

--- public/api/index.php
+++ public/api/index.php
@@ -3,18 +3,24 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
+// Pastikan waktu mulai didefinisikan
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// Register the Composer autoloader...
+// Menggunakan autoloader dari composer
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
+// Bootstrap Laravel
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+// Menangani request
+$request = Request::capture();
+
+// Penting untuk Vercel: Laravel perlu tahu jika berjalan di balik proxy
+$app->prepareRequestForFramework($request);
+
+$response = $app->handle($request);
+
+$response->send();
+
+$app->terminate($request, $response);
